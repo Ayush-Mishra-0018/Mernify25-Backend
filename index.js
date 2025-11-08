c65eea920516eeeb1dcb51f1e1b6e412a2ca5dac
iHarshSinha <harsh.sinha@iiitb.ac.in>
2025-11-08 09:22:51 +0530

    mongo

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ app.use(cors({
   exposedHeaders: ["x-updated-token"]
 }));
 
-mongoose.connect(process.env.MONGODB_URI);
-let db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("Connected to MongoDB");
-});
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ Connected to MongoDB"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
 
 const port = process.env.BACKEND_PORT || 5000;
 
